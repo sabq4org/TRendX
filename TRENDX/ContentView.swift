81525f970890331d670f6e1ee968d5a992553a0a
mizan <info@sabq.org>
2026-04-16 16:05:23 +0300

    Initial Commit

---
+++ TRENDX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TRENDX
+//
+//  Created by Ali Alhazmi on 16/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TRENDX/TRENDXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRENDXApp.swift
+//  TRENDX
+//
+//  Created by Ali Alhazmi on 16/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct TRENDXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
